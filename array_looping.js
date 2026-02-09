2b762e83289558e589fbe03e1f263e78a7039e21
sheikh-muzammil2026 <discordsstudent@gmail.com>
2026-02-09 21:59:50 +0600

    added axtra practice on module 20.5

--- array_looping.js
+++ array_looping.js
@@ -1,9 +1,83 @@
+/**
+ * reverse এর বিকল্প = unshift দিয়ে লুপ চালানো।
 let numbers = [1,2,3,4,5,6,7];
-
 let result =[];
-
 for(const num of numbers){
     result.unshift(num);
 }
-
 console.log(result);
+
+*/
+
+
+// let numbers = [11,21,32,45,53,60,74];
+
+// let result =[];
+
+// for(const num of numbers){
+//     result.unshift(num);
+// }
+
+// console.log(result);
+
+// for(let x = numbers.length -1; x >= 0; x--){  // array-এর শেষ index থেকে শুরু করে প্রথম index পর্যন্ত যাবে
+//      result.push(numbers[x]);
+// }
+// console.log(numbers[6]);
+
+
+/**
+ * Task 3
+Use a for...of loop to concatenate all the elements of an array into a single string.
+
+Input: var numbers = ['Tom', 'Tim', 'Tin', 'Tik']
+
+Output:
+
+'TomTimTinTik'
+
+*/
+let sentence = ['Tom', 'Tim', 'Tin', 'Tik'];
+let resultString = [];
+for (const str of sentence){
+     resultString += str;
+}
+
+console.log(resultString);
+
+
+/**
+ * 
+Task 4 (Hard)
+Reverse the words of a sentence. Only the position of the word will be reversed. check out the output
+
+Input: const statement = 'I am a hard working person'
+
+Output:
+
+'person working hard a am I'
+*/
+
+// let str = 'I am a hard working person';
+// let getSpase = str.split(" ");  // split(" ") → sentence কে স্পেস এর ভিত্তিতে word-array বানিয়েছে
+// let reversed = getSpase.reverse(); // word গুলোর position উল্টেছে
+// let result = reversed.join(" ");  // split(" ") এরে ছিল। এখন আবার সুন্দর স্পেস এর ভিত্তিতে sentence বানিয়েছে । যদি " " দেয়া না হত তাহলে প্রত্যেক শব্দের পরে কমা বসিয়ে দিত। 
+// console.log(result);
+// for (const str1 of reversed){
+//      result.reverse(str1);
+    
+// }
+// //  resultr = reversed.reverse();
+// console.log(result);
+
+     let str = 'I am a hard working person';
+     let getSpase = str.split(" ");
+     let result =[];
+     console.log(getSpase);
+     for(const word of  getSpase){
+          result.reverse().push(word);
+     }
+     console.log(result);
+     let reversed = result.reverse();
+     // result.reverse is not a function 👉 এই এররটা আসে যখন তুমি এমন একটা ভ্যারিয়েবলের ওপর .reverse() চালাও, যেটা Array না।
+     console.log(reversed);
---
+++ module_20-5.js
@@ -0,0 +1,108 @@
+/**
+ *   ## 🧩 Full Practice Problem–01: Student Management System
+
+### 🎯 ডাটা স্ট্রাকচার
+
+একটি `student` object বানাতে হবে, যেখানে থাকবে—
+
+* `name` (string)
+* `roll` (number)
+* `isRegular` (boolean)
+* `subjects` (array of strings)
+* `marks` (object)
+
+  * `bangla`
+  * `english`
+  * `math`
+* `friends` (array of objects)
+
+  * `{ name, roll }`
+* `getAverage` নামে একটি method
+
+### ✅ করতে হবে
+
+1. loop ব্যবহার করে সব `subjects` প্রিন্ট করো
+2. `friends` array থেকে শুধু friend-এর নাম প্রিন্ট করো
+3. `getAverage()` method কল করলে
+   👉 তিনটা সাবজেক্টের average রিটার্ন করবে
+4. যদি average ≥ 40 হয়
+   👉 `"Passed"` প্রিন্ট করো, না হলে `"Failed"`
+
+*/
+
+let student = {
+     name : "ahmad",
+     roll : 13,
+     isRegular : true,
+     subjects : [ "Bangla", "Arabic", "English"],
+     marks : {
+        Bangla: 44,
+        Arabic : 55,
+        English : 66
+
+     },
+     friends : [
+        {name : "shakil", roll: 22},
+        {name : "farhan", roll: 34},
+        {name : "labib", roll: 67}
+     ],
+    //  getAverage : function (marks){
+       
+    //     let totalMark =0;
+    //     for( const subj in marks){
+    //         totalMark += marks[subj];
+    //     }
+    //    return totalMark / Object.keys(marks).length;
+
+    //  }
+    getAverage : function (){
+       
+        let totalMark =0;
+        let average =0;
+        let position ="";
+        for( const subj in this.marks){
+            totalMark += this.marks[subj];
+        }
+                 average = totalMark / Object.keys(this.marks).length;
+                 if(average >= 40){
+                    position = "passed";
+                 }else {
+                    position = "failed";
+                 }
+
+        return {
+             average,
+             position,
+        };
+     },
+
+      showReport : function (){
+
+        // subjects print
+    
+        let subjects_names =[];
+        let friendsNames =[];
+        for (const sub of this.subjects){
+            subjects_names.push(sub);
+        }
+         let subNames = subjects_names.join(", ") ;
+
+        for(const f in this.friends){
+            friendsNames.push(f);
+        }
+        let FNames = friendsNames.join(", ") ;
+        return {
+            subNames,
+            FNames
+
+        }
+        
+      }
+     
+
+};
+
+// console.log(student.getAverage(student.marks));
+
+console.log("Subject names : "+JSON.stringify(student.showReport()));
+console.log("Published result : "+JSON.stringify(student.getAverage()));
